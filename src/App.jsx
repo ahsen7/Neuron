28d390440c6a3ec0dc202df5726e555d4331d92a
Muhammad Ahsen <ahsen.mehmood412@gmail.com>
2025-01-16 21:44:29 +0500

    Project Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
+import Benefits from './components/Benefits/Benefits'
+import Collaboration from './components/Collaboration/Collaboration'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
+import Pricing from './components/Pricing/Pricing'
+import Roadmap from './components/Roadmap/Roadmap'
+import Services from './components/Services/Services'
 
 const App = () => {
 
@@ -10,6 +16,12 @@ const App = () => {
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
        <Header/>
        <Hero/>
+       <Benefits/>
+       <Collaboration/>
+       <Services/>
+       <Pricing/>
+       <Roadmap/>
+       <Footer/>
       </div>
       <ButtonGradient />
     </>
